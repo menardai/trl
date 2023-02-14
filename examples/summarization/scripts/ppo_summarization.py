6b3795a1d48304405552de3631a3e9d27f7be509
Stephane Menard <stephane@heyday.ai>
2023-02-14 10:08:21 -0500

    initial ppo summarization commit

---
+++ examples/summarization/scripts/ppo_summarization.py
@@ -0,0 +1,182 @@
+# coding=utf-8
+# Copyright 2022 The HuggingFace Inc. team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import torch
+
+from tqdm import tqdm
+
+tqdm.pandas()
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+from trl import PPOTrainer2GPU, PPOConfig, AutoModelForCausalLMWithValueHead
+from trl.core import LengthSampler
+
+from summarization_dataset import build_dataset
+
+
+########################################################################
+# This is a fully working simple example to use trl with accelerate.
+#
+# Fine-tunes a GPT2 model on the summarization dataset using PPO
+# (proximal policy optimization) using 2 GPUs.
+#
+# First initialize accelerate configuration with `accelerate config`
+# and select fp16
+#
+########################################################################
+
+# We first define the configuration of the experiment, defining the model, the dataset,
+# the training parameters, and the PPO parameters.
+
+# Check the default arguments in the `PPOConfig` class for more details.
+# If you want to log with tensorboard, add the kwarg
+# `accelerator_kwargs={"logging_dir": PATH_TO_LOGS}` to the PPOConfig.
+config = PPOConfig(
+    model_name="gpt2",
+    learning_rate=1.41e-5,
+    batch_size=1,
+)
+
+
+def collator(data):
+    return dict((key, [d[key] for d in data]) for key in data[0])
+
+
+tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+# GPT-2 tokenizer has a pad token, but it is not eos_token by default. We need to set it to eos_token.
+# (only for this model)
+tokenizer.pad_token = tokenizer.eos_token
+
+train_dataset, train_prompts, val_prompts, prompt_summary_dict = build_dataset(tokenizer)
+
+# Now let's build the model, the reference model, and the tokenizer.
+logging.warning("Loading Model and Reference Model...")
+model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
+ref_model = AutoModelForCausalLMWithValueHead.from_pretrained(config.model_name)
+
+# We then build the PPOTrainer, passing the model, the reference model, the tokenizer
+ppo_trainer = PPOTrainer2GPU(
+    config,
+    model,
+    ref_model,
+    tokenizer,
+    dataset=train_dataset,
+    data_collator=collator,
+)
+
+# ---------------------------------------
+# RM Model
+# ---------------------------------------
+
+logging.warning("Loading Reward Model...")
+rw_model_name = "Tristan/gpt2_reward_summarization"
+rw_device = "cpu"
+
+# Load the pre-trained reward model
+rw_tokenizer = AutoTokenizer.from_pretrained(rw_model_name)
+rw_model = AutoModelForSequenceClassification.from_pretrained(rw_model_name, num_labels=1)
+rw_model.eval()
+# rw_model.half()
+
+# Need to do this for gpt2, because it doesn't have an official pad token.
+rw_tokenizer.pad_token = rw_tokenizer.eos_token
+rw_model.config.pad_token_id = rw_tokenizer.eos_token_id
+
+
+def get_scores(samples):
+    """Compute scores for the given list of string."""
+    scores_list = []
+    batch_size = 2
+    for i in range(0, len(samples), batch_size):
+        sub_samples = samples[i: i + batch_size]
+
+        encodings_dict = rw_tokenizer(
+            sub_samples,
+            truncation=True,
+            max_length=550,  # max_length=config.train.seq_length,
+            padding="max_length",
+            return_tensors="pt",
+        )
+        input_ids = encodings_dict["input_ids"].to(rw_device)
+        attn_masks = encodings_dict["attention_mask"].to(rw_device)
+        # input_ids = input_ids.repeat(2, 1)    <-- pas besoin avec le trl rw model, c'etait pour trlx rw model?
+        # attn_masks = attn_masks.repeat(2, 1)
+
+        with torch.no_grad():
+            sub_scores = rw_model(input_ids=input_ids, attention_mask=attn_masks)[0]
+
+        scores_list.append(sub_scores[0])
+
+    scores = torch.cat(scores_list, dim=0)
+    return scores
+
+
+def reward_fn(samples):
+    """Compute rewards for the given list of string."""
+    original_samples = [text.split("TL;DR:")[0] + "TL;DR: " for text in samples]
+    original_samples = [text + prompt_summary_dict[text.strip()] for text in original_samples]
+
+    original_scores = get_scores(original_samples)
+    scores = get_scores(samples)
+
+    norms_scores = scores - original_scores
+    return norms_scores
+
+
+# ---------------------------------------
+# PPO Training
+# ---------------------------------------
+logging.warning("Starting PPO Training...")
+
+# We then define the arguments to pass to the `generate` function.
+# These arguments are passed to the `generate` function of the PPOTrainer,
+# which is a wrapper around the `generate` function of the trained model.
+generation_kwargs = {
+    "min_length": -1,
+    "top_k": 0.0,
+    "top_p": 1.0,
+    "do_sample": True,
+    "pad_token_id": tokenizer.eos_token_id
+}
+output_min_length = 8
+output_max_length = 50
+output_length_sampler = LengthSampler(output_min_length, output_max_length)
+
+for epoch, batch in tqdm(enumerate(ppo_trainer.dataloader)):
+    query_tensors = batch['input_ids']
+
+    # --- Get response from gpt2 ---
+    response_tensors = []
+    for query in query_tensors:
+        gen_len = output_length_sampler()
+        generation_kwargs["max_new_tokens"] = gen_len
+
+        response = ppo_trainer.generate(query, **generation_kwargs)
+        response_tensors.append(response.squeeze()[-gen_len:])
+
+    batch['response'] = [tokenizer.decode(r.squeeze()) for r in response_tensors]
+
+    # --- Compute rewards ---
+    texts = [q + r for q, r in zip(batch['query'], batch['response'])]
+    rewards = reward_fn(texts)
+    reward_tensors = [torch.tensor(reward) for reward in rewards]
+    # pipe_outputs = sentiment_pipe(texts, **sent_kwargs)
+    # rewards = [torch.tensor(output[1]["score"]) for output in pipe_outputs]
+
+    # --- Run a PPO step ---
+    stats = ppo_trainer.step(query_tensors, response_tensors, reward_tensors)
+
+    ppo_trainer.log_stats(stats, batch, rewards)
---
+++ examples/summarization/scripts/summarization_dataset.py
@@ -0,0 +1,134 @@
+import logging
+
+from tqdm import tqdm
+
+from datasets import Dataset, load_dataset
+from transformers import AutoTokenizer
+
+
+def get_prompt_dataset(tokenizer, prompts, max_length):
+    """
+    Get the prompt after decoding to make sure dictionary of prompts and summaries is consistent decode prompt
+    """
+    formatted_prompts = []
+    for i in tqdm(range(len(prompts))):
+        tmp = tokenizer.decode(
+            tokenizer(
+                prompts[i].split("TL;DR:")[0],
+                truncation=True,
+                max_length=max_length - 5,  # to make sure "TL;DR" don't get truncated
+            )["input_ids"],
+            skip_special_tokens=True,
+        ).strip()
+
+        tmp = tmp + "\nTL;DR:"
+        tmp = tokenizer.decode(
+            tokenizer(tmp, truncation=True, max_length=max_length)["input_ids"],
+            skip_special_tokens=True,
+        ).strip()
+
+        formatted_prompts.append(tmp)
+
+    return formatted_prompts
+
+
+def build_dataset(tokenizer):
+    tokenizer.padding_side = "left"
+
+    max_length_input = (
+        # config.train.seq_length - config.method.gen_kwargs["max_new_tokens"]
+        550 - 50
+    )
+
+    # Browse dataset content here:
+    # https://huggingface.co/datasets/CarperAI/openai_summarize_tldr
+    #
+    # DatasetDict({
+    #     train: Dataset({
+    #         features: ['prompt', 'label'],
+    #         num_rows: 116722
+    #     })
+    #     valid: Dataset({
+    #         features: ['prompt', 'label'],
+    #         num_rows: 6447
+    #     })
+    #     test: Dataset({
+    #         features: ['prompt', 'label'],
+    #         num_rows: 6553
+    #     })
+    # })
+    dataset = load_dataset("CarperAI/openai_summarize_tldr")
+
+    # Store data into prompt and label pairs
+    # val_set {list: 6447}:
+    #   [
+    #     (
+    #       "SUBREDDIT: r/AskReddit\nTITLE: How do you get someone out of your head?\nPOST: Hi,\nI'm 22, and I have been with my girlfriend for 5 years now. We recently moved together. We've always loved each other intensely.\n\nProblem, I recently started to have feelings for an other person (a friend). This person has had a boyfriend for now 3 years, and has absolutely no ideas. Those feelings were so strong, it was hard to hide them. After 2 months of me being distant and really sad, my girlfriend forced me to say what was bothering me. I'm not a good liar, and now she knows.\n\nWe decided to give us a week alone, I went to my parents. \n\nNow, I'm completely lost. I keep on thinking about this person, and I hate that. I would like for those feelings to go away, to leave me alone. But I can't.  \n\nWhat do I do? It's been 3 months now, and I'm just desperate.\nTL;DR: ",
+    #       "long relationship; fell in love with an other person; admitted it; would like it to disappear, though it doesn't."
+    #     ),
+    #     (
+    #       "SUBREDDIT: r/pettyrevenge\nTITLE: So, my mom woke me up with a loud TV.\nPOST: She was in her living room, watching TV. This was at about 8:30 in the morning, and she was exercising. She turned the TV up extra loud to hear it over her excercycle, and woke me up. I went in there asking for her to turn it down. She said she didn't have to; I explained that I always used headphones so she didn't have to deal with my noise and that she should give me a little more respect, given that I paid rent at the time.\n\nShe disagreed. I went back to my room, rather pissed off at the lack of equality. I had no lock on my door; but I had a dresser right next to it, so I pulled one of the drawers out enough so that it caused the door to not be openable. Then, I turned my speakers up really loud and blasted Gangnam Style on repeat, with the bass cranked up as high as it could go.\n\nIf you hate Gangnam Style for being overplayed, you will see why I chose that particular song. I personally don't mind it. But here's the thing about my bass; it vibrates the walls, making one hell of a lot of noise. Needless to say, my mom was not pleased and shut off the internet. But it was oh so worth it.\nTL;DR: ",
+    #       "Mom had the TV on loud and woke me up, didn't care that I'd respected audio levels in the house, so I countered with playing Gangnam Style on repeat with the bass thumping through the walls."
+    #     ),
+    #     ...
+    #   ]
+    train_set = [(sample["prompt"], sample["label"]) for sample in dataset["train"]]
+    val_set = [(sample["prompt"], sample["label"]) for sample in dataset["valid"]]
+
+    # Split contents into summaries and labels
+    #
+    # val_posts {tuple: 6447}:
+    #   (
+    #     "SUBREDDIT: r/AskReddit\nTITLE: How do you get someone out of your head?\nPOST: Hi,\nI'm 22, and I have been ... just desperate.\nTL;DR: ",
+    #     "SUBREDDIT: r/pettyrevenge\nTITLE: So, my mom woke me up with a loud TV.\nPOST: She was in her living room, ... so worth it.\nTL;DR: ",
+    #     ...
+    #   )
+    #
+    # val_summaries {tuple: 6447}:
+    #   (
+    #     "long relationship; fell in love with an other person; admitted it; would like it to disappear, though it doesn't."
+    #     "Mom had the TV on loud and woke me up, didn't care that I'd respected audio levels in the house, so I countered with playing Gangnam Style on repeat with the bass thumping through the walls."
+    #     ...
+    #   )
+    train_posts, train_summaries = zip(*train_set)
+    val_posts, val_summaries = zip(*val_set)
+
+    # Get the OpenAI summaries.
+    #   - val_prompts:
+    #     a copy of val_posts clipped to max_length_input with a guarantee to end with "\nTL;DR:"
+    #
+    #   - prompt_summary_dict (contains both train and val prompts):
+    #     {
+    #       "SUBREDDIT: r/AskReddit\nTITLE: How do you get someone out of your head?\nPOST: Hi,\nI'm 22, and I have been ... just desperate.\nTL;DR:": "long relationship; fell in love with an other person; admitted it; would like it to disappear, though it doesn't.",
+    #       "SUBREDDIT: r/pettyrevenge\nTITLE: So, my mom woke me up with a loud TV.\nPOST: She was in her living room, ... so worth it.\nTL;DR:": "Mom had the TV on loud and woke me up, didn't care that I'd respected audio levels in the house, so I countered with playing Gangnam Style on repeat with the bass thumping through the walls."
+    #       ...
+    #     }
+    prompt_summary_dict = {}
+
+    train_posts = train_posts[:100]    # ----> DEBUG DEBUG DEBUG DEBUG DEBUG <-----
+    val_posts = val_posts[:10]         # ----> DEBUG DEBUG DEBUG DEBUG DEBUG <-----
+
+    logging.warning("Formatting train prompts...")
+    train_prompts = get_prompt_dataset(tokenizer, train_posts, max_length_input)
+    for i in range(len(train_prompts)):
+        prompt_summary_dict[train_prompts[i]] = train_summaries[i]
+
+    logging.warning("Formatting eval prompts...")
+    val_prompts = get_prompt_dataset(tokenizer, val_posts, max_length_input)
+    for i in range(len(val_prompts)):
+        prompt_summary_dict[val_prompts[i]] = val_summaries[i]
+
+    # tokenized the training dataset
+    def tokenize(sample):
+        sample["input_ids"] = tokenizer.encode(sample["prompt"])
+        sample["query"] = tokenizer.decode(sample["input_ids"])
+        return sample
+
+    logging.warning("Tokenizing training set...")
+    train_dataset = Dataset.from_dict(
+        {"prompt": train_prompts}
+    )
+    train_dataset = train_dataset.map(tokenize, batched=False)
+    train_dataset.set_format(type='torch')
+
+    return train_dataset, train_prompts, val_prompts, prompt_summary_dict
